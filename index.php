5401ecaad55bb24a6401769cf0b6423ed1a1a848
Yoga Hanggara <yogahanggara@gmail.com>
2013-04-13 23:00:36 +0700

    percobaan

--- index.php
+++ index.php
@@ -1,3 +1,72 @@
 <?php
-	get_header();
+get_header();
 ?>
+
+<div class="container-narrow">
+
+	<div class="masthead">
+		<ul class="nav nav-pills pull-right">
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+		<h3 class="muted">Project name</h3>
+	</div>
+
+	<hr>
+
+	<div class="jumbotron">
+			<img src="/loremphoto/timthumb.php?h=300&w=700&src=/loremphoto/photo/6.jpg" style="width:100%" />
+	</div>
+
+	<hr>
+
+	<div class="row-fluid">
+		<div class="span6">
+			<img src="/loremphoto/timthumb.php?h=150&w=400&src=/loremphoto/photo/4.jpg" style="width:100%" />
+			<h4>Subheading</h4>
+			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+		</div>
+
+		<div class="span6">
+			<img src="/loremphoto/timthumb.php?h=150&w=400&src=/loremphoto/photo/5.jpg" style="width:100%" />
+			<h4>Subheading</h4>
+			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+		</div>
+	</div>
+
+	<div class="row-fluid">
+		<div class="span6">
+			<img src="/loremphoto/timthumb.php?h=150&w=400&src=/loremphoto/photo/3.jpg" style="width:100%" />
+			<h4>Subheading</h4>
+			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+		</div>
+
+		<div class="span6">
+			<img src="/loremphoto/timthumb.php?h=150&w=400&src=/loremphoto/photo/6.jpg" style="width:100%" />
+			<h4>Subheading</h4>
+			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+<ul class="pager">
+  <li class="previous">
+    <a href="#">&larr; Older</a>
+  </li>
+  <li class="next">
+    <a href="#">Newer &rarr;</a>
+  </li>
+</ul>
+</div>
+</div>
+
+	<hr>
+
+	<div class="footer">
+		<p>&copy; Company 2013</p>
+	</div>
+
+    </div> <!-- /container -->
+
+    <?php get_footer(); ?>
