b9778ec7b8e885458512807a67665212b121e71a
Yoga Hanggara <yogahanggara@gmail.com>
2013-04-14 17:19:14 +0700

    remove breadcrumb

--- header.php
+++ header.php
@@ -79,9 +79,3 @@
 	<hr>
 <?php endif; ?>
 
-	<ul class="breadcrumb">
-		<li><a href="/">Home</a> <span class="divider">/</span></li>
-		<li><a href="#">Library</a> <span class="divider">/</span></li>
-		<li class="active">Data</li>
-	</ul>
-
