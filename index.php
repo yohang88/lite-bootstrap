2f42e27bc3ec484a791a46d2abd65d9fb538c1d5
Yoga Hanggara <yogahanggara@gmail.com>
2013-04-14 11:54:20 +0700

    using while post

--- index.php
+++ index.php
@@ -10,63 +10,56 @@ get_header();
 			<li><a href="#">About</a></li>
 			<li><a href="#">Contact</a></li>
 		</ul>
-		<h3 class="muted">Project name</h3>
+		<h3 class="muted">My Website Name</h3>
 	</div>
 
 	<hr>
 
 	<div class="jumbotron">
-			<img src="/loremphoto/timthumb.php?h=300&w=700&src=/loremphoto/photo/6.jpg" style="width:100%" />
+		<img src="/loremphoto/timthumb.php?h=422&w=700&src=/loremphoto/photo/header.jpg" />
 	</div>
 
 	<hr>
 
-	<div class="row-fluid">
-		<div class="span6">
-			<img src="/loremphoto/timthumb.php?h=150&w=400&src=/loremphoto/photo/4.jpg" style="width:100%" />
-			<h4>Subheading</h4>
-			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-		</div>
+	<ul class="breadcrumb">
+	  <li><a href="#">Home</a> <span class="divider">/</span></li>
+	  <li><a href="#">Library</a> <span class="divider">/</span></li>
+	  <li class="active">Data</li>
+	</ul>
 
-		<div class="span6">
-			<img src="/loremphoto/timthumb.php?h=150&w=400&src=/loremphoto/photo/5.jpg" style="width:100%" />
-			<h4>Subheading</h4>
-			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-		</div>
-	</div>
-
+	<?php
+	$m = 1;
+	?>
 	<div class="row-fluid">
+		<?php while($m <= 6): ?>
 		<div class="span6">
-			<img src="/loremphoto/timthumb.php?h=150&w=400&src=/loremphoto/photo/3.jpg" style="width:100%" />
-			<h4>Subheading</h4>
+			<img src="/loremphoto/timthumb.php?h=150&w=350&src=/loremphoto/photo/<?php echo rand(1,6) ?>.jpg" />
+			<h4><a href="#">Subheading <?php echo $m ?></a></h4>
 			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
 		</div>
-
-		<div class="span6">
-			<img src="/loremphoto/timthumb.php?h=150&w=400&src=/loremphoto/photo/6.jpg" style="width:100%" />
-			<h4>Subheading</h4>
-			<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-		</div>
-	</div>
-	<div class="row-fluid">
-		<div class="span12">
-<ul class="pager">
-  <li class="previous">
-    <a href="#">&larr; Older</a>
-  </li>
-  <li class="next">
-    <a href="#">Newer &rarr;</a>
-  </li>
-</ul>
-</div>
+		<?php $m++; ?>
+	<?php endwhile; ?>
 </div>
 
-	<hr>
-
-	<div class="footer">
-		<p>&copy; Company 2013</p>
+<div class="row-fluid">
+	<div class="span12">
+		<ul class="pager">
+			<li class="previous">
+				<a href="#">&larr; Older</a>
+			</li>
+			<li class="next">
+				<a href="#">Newer &rarr;</a>
+			</li>
+		</ul>
 	</div>
+</div>
 
-    </div> <!-- /container -->
+<hr>
 
-    <?php get_footer(); ?>
+<div class="footer">
+	<p>&copy; Company 2013</p>
+</div>
+
+</div> <!-- /container -->
+
+<?php get_footer(); ?>
